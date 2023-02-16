7053ee47228a579a5e2dccd17bb508e2c6dbc83e
Pedro Reynoso <113930313+preynoso3@users.noreply.github.com>
2023-02-16 18:40:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# remember to fork this and push to github
+
+# f = open('test.txt', 'a')
+# f.write(' ')
+# # read gices us everything
+# # readline gives us the first line
+# # print(f.readline())
+# f.close()
+
+# f = open('employees.txt', 'a')
+# f.write(' ')
+# f.close()
+
+# with open('test.txt', 'r') as f:
+#   # f_contents= f.read()
+#   # f.write('bgpurbgergeuor')
+#   # print(f)
+
+#   for line in f:
+#     print(line, end='')
+
+with open('period7.txt', 'w') as f:
+  f.write("this mess is over")
